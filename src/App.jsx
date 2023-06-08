2fc37eae1bbc9e5a4378fcc39527312d75cd8a07
Carmen <path4design@gmail.com>
2023-06-08 14:19:17 +0200

    finish iteration 4

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,24 @@
 import "./App.css";
+import FoodBox from "./components/FoodBox";
+import foodsJson from "./foods.json";
+import { useState } from "react";
 
 function App() {
+  const [foods, setFoods] = useState([...foodsJson]);
+
+  function handleDelete(id) {
+    setFoods(foods.filter((f) => f.id !== id));
+  }
+
   return (
     <div className="App">
       <h1>LAB | React IronNutrition</h1>
+      <div className="card-container">
+        {/** Iterate over the foods array and render a <FoodBox /> component for each individual food item. */}
+        {foods.map((f) => (
+          <FoodBox foods={f} key={f.id} onDelete={handleDelete}/>
+        ))}
+      </div>
     </div>
   );
 }
--- src/components/FoodBox.jsx
+++ src/components/FoodBox.jsx
@@ -1,1 +1,23 @@
 // Your code here
+
+export default function FoodBox({ foods, onDelete  }) {
+
+  return (
+    <>
+      <div className="card">
+        <p>{foods.name}</p>
+
+        <img src={foods.image} />
+
+        <p>Calories: {foods.calories}</p>
+        <p>Servings {foods.servings}</p>
+
+        <p>
+          <b>Total Calories: {foods.servings * foods.calories} </b> kcal
+        </p>
+
+        <button onClick={() => onDelete(foods.id)}>Delete</button>
+      </div>
+    </>
+  );
+}
